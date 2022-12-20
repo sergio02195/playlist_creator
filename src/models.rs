c3b6316a5680c10847694a2d28056a3c132285e2
sergio c <sergio-coelho20111@hotmail.com>
2022-12-20 12:23:07 -0300

    add models

---
+++ src/models.rs
@@ -0,0 +1,34 @@
+use deadpool_lapin::{PoolError};
+use serde::{Serialize, Deserialize};
+use std::result::Result as StdResult;
+use thiserror::Error as ThisError;
+use warp::{Rejection};
+// use chrono::DateTime;
+
+#[derive(Serialize,Deserialize)]
+pub struct User {
+    pub id: String,
+    pub name: String,
+}
+
+// #[derive(Serialize,Deserialize)]
+// pub struct StartData {
+//     pub origin: DateTime,
+//     pub token: String,
+// }
+
+pub type WebResult<T> = StdResult<T, Rejection>;
+pub type RMQResult<T> = StdResult<T, PoolError>;
+pub type Result<T> = StdResult<T, Error>;
+
+pub type Connection = deadpool::managed::Object<deadpool_lapin::Manager>;
+
+#[derive(ThisError, Debug)]
+pub enum Error {
+    #[error("rmq error: {0}")]
+    RMQError(#[from] lapin::Error),
+    #[error("rmq pool error: {0}")]
+    RMQPoolError(#[from] PoolError),
+}
+
+impl warp::reject::Reject for Error {}
