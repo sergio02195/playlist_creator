64eb5866badacd01f197cb47787fcbab59348c5c
sergio c <sergio-coelho20111@hotmail.com>
2022-12-24 15:14:13 -0300

    start consumer at core

--- src/core.rs
+++ src/core.rs
@@ -1,14 +1,18 @@
 use futures::join;
 use http_health_check::HTTPHealthCheck;
+use rabbitmq::RabbitMQClient;
 
 mod http_health_check;
 mod models;
+mod rabbitmq;
 
 #[tokio::main]
 async fn main() {
     println!("Started server at localhost:8000");
     let health_check = HTTPHealthCheck::new();
+    let rabbitmc_consumer = RabbitMQClient::new("amqp://guest:guest@localhost:5672//".to_string(), "".to_string() );
     let _ = join!(
-        health_check.start()
+        health_check.start(),
+        rabbitmc_consumer.consume()
     );
 }
