9b64edb3d54e2e14d6097191e89694bd63f9755b
sergio c <sergio-coelho20111@hotmail.com>
2022-12-24 15:13:49 -0300

    fix rabbitmq consumer

--- src/rabbitmq.rs
+++ src/rabbitmq.rs
@@ -1,9 +1,15 @@
-
-use deadpool_lapin::{Manager, Pool, PoolError, lapin::{ConnectionProperties, options::{QueueDeclareOptions, BasicConsumeOptions, BasicAckOptions}, types::FieldTable}};
+use crate::models::{Connection, RMQResult, Result};
+use deadpool_lapin::{
+    lapin::{
+        options::{BasicAckOptions, BasicConsumeOptions, QueueDeclareOptions},
+        types::FieldTable,
+        ConnectionProperties,
+    },
+    Manager, Pool,
+};
+use futures::StreamExt;
 use std::time::Duration;
 use tokio_amqp::*;
-use async_trait::async_trait;
-use crate::models::{RMQResult,Connection};
 
 pub struct RabbitMQClient {
     queue: String,
@@ -12,7 +18,7 @@ pub struct RabbitMQClient {
 }
 
 impl RabbitMQClient {
-    fn new(addr: String, queue: String) -> Self {
+    pub fn new(addr: String, queue: String) -> Self {
         Self {
             queue: queue,
             addr: addr,
@@ -20,71 +26,73 @@ impl RabbitMQClient {
         }
     }
 
-    async fn consume(&self) {
-
-        let manager = Manager::new(self.addr, ConnectionProperties::default().with_tokio());
+    pub async fn consume(&self) -> Result<()> {
+        let manager = Manager::new(&self.addr, ConnectionProperties::default().with_tokio());
         let pool: Pool = deadpool::managed::Pool::builder(manager)
             .max_size(10)
             .build()
             .expect("can create pool");
-        
-        let rmq_con = Self::get_rmq_con(pool).await.map_err(|e| {
-            eprintln!("could not get rmq con: {}", e);
-            e
-        })?;
+
+        self.rmq_listen(pool).await
     }
 
-    async fn rmq_listen(pool: Pool) -> Result<()> {
+    async fn rmq_listen(&self, pool: Pool) -> Result<()> {
         let mut retry_interval = tokio::time::interval(Duration::from_secs(5));
         loop {
             retry_interval.tick().await;
             println!("connecting rmq consumer...");
-            match Self::init_rmq_listen(pool.clone()).await {
+            match self.init_rmq_listen(pool.clone()).await {
                 Ok(_) => println!("rmq listen returned"),
                 Err(e) => eprintln!("rmq listen had an error: {}", e),
             };
         }
     }
-    
-    async fn init_rmq_listen(pool: Pool) -> Result<()> {
-        let rmq_con = Self::get_rmq_con(pool).await.map_err(|e| {
+
+    async fn get_rmq_con(&self, pool: Pool) -> RMQResult<Connection> {
+        let connection = pool.get().await?;
+        Ok(connection)
+    }
+
+    async fn init_rmq_listen(&self, pool: Pool) -> Result<()> {
+        let rmq_con = self.get_rmq_con(pool).await.map_err(|e| {
             eprintln!("could not get rmq con: {}", e);
             e
         })?;
         let channel = rmq_con.create_channel().await?;
-    
+
         let queue = channel
             .queue_declare(
-                "hello",
+                &self.queue,
                 QueueDeclareOptions::default(),
                 FieldTable::default(),
             )
             .await?;
         println!("Declared queue {:?}", queue);
-    
+
         let mut consumer = channel
             .basic_consume(
-                "hello",
+                &self.queue,
                 "my_consumer",
                 BasicConsumeOptions::default(),
                 FieldTable::default(),
             )
             .await?;
-    
+
         println!("rmq consumer connected, waiting for messages");
         while let Some(delivery) = consumer.next().await {
             if let Ok((channel, delivery)) = delivery {
                 println!("received msg: {:?}", delivery);
+                let s = match std::str::from_utf8(&delivery.data) {
+                    Ok(v) => v,
+                    Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+                };
+            
+                println!("result: {}", s);
                 channel
                     .basic_ack(delivery.delivery_tag, BasicAckOptions::default())
                     .await?
             }
         }
         Ok(())
-    }
-
-    async fn get_rmq_con(pool: Pool) -> RMQResult<Connection> {
-        let connection = pool.get().await?;
-        Ok(connection)
     }
 }
